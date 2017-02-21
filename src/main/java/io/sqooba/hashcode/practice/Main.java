a2d51af0d784ef368ca20f1063b2d7509ff66682
Gazzola, Valentin <valentin@sqooba.io>
2017-02-21 16:12:31 +0100

    Initial structure with Maven

---
+++ src/main/java/io/sqooba/hashcode/practice/Main.java
@@ -0,0 +1,7 @@
+package io.sqooba.hashcode.practice;
+
+/**
+ * Created by VGazzola on 21/02/17.
+ */
+public class Main {
+}
---
+++ src/main/java/io/sqooba/hashcode/work/Main.java
@@ -0,0 +1,7 @@
+package io.sqooba.hashcode.work;
+
+/**
+ * Created by VGazzola on 21/02/17.
+ */
+public class Main {
+}
